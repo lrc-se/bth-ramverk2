5dd1df6bad59f70abfa5dec157ef25546164c538
Kalle Brisland <kabc16@student.bth.se>
2017-10-28 19:49:04 +0200

    Added EJS base

--- index.js
+++ index.js
@@ -3,17 +3,42 @@
 "use strict";
 
 const express = require("express");
+//const ejs = require("ejs");
 const path = require("path");
 
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
+
+
+function renderLayout(res, view, title, data) {
+    res.render("layout", {
+        view: view,
+        title: title || "",
+        data: data || {}
+    });
+}
+
+
+app.get("/", function(req, res) {
+    renderLayout(res, "index", "Kalles sida");
+});
+
+app.get("/about", function(req, res) {
+    renderLayout(res, "about", "Om webbplatsen");
+});
+
+app.get("/report", function(req, res) {
+    renderLayout(res, "report", "Redovisningar");
+});
 
 app.use(function(req, res, next) {
     var err = new Error("404 Not Found");
     err.status = 404;
     next(err);
 });
+
 
 var port = (!isNaN(process.env.DBWEBB_PORT) ? +process.env.DBWEBB_PORT : 1337);
 app.listen(port);
