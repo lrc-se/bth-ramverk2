149358415b396e5ab5f46800d3abda6e910128a2
Kalle Brisland <kabc16@student.bth.se>
2017-11-17 23:13:45 +0100

    Added assertion messages

--- test/example.js
+++ test/example.js
@@ -12,6 +12,6 @@ tap.test("Test age calculator", function(t) {
     t.plan(2);
     
     let year = new Date().getFullYear();
-    t.equal(util.getAge(year - 47), 47);
-    t.equal(util.getAge(), year - 1983);
+    t.equal(util.getAge(year - 47), 47, "age should be 47 (birthyear " + (year - 47) + " provided)");
+    t.equal(util.getAge(), year - 1983, "age should be " + (year - 1983) + " (default birthyear 1983)");
 });
